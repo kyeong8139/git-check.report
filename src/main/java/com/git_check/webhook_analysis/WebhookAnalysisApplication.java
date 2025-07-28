53766ac2986baf0b50869101402fb796791e1145
bokyeong-Lee <kyeong8139@gmail.com>
2025-07-28 18:48:57 +0900

    init: init project

---
+++ src/main/java/com/git_check/webhook_analysis/WebhookAnalysisApplication.java
@@ -0,0 +1,13 @@
+package com.git_check.webhook_analysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebhookAnalysisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebhookAnalysisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/git_check/webhook_analysis/WebhookAnalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.git_check.webhook_analysis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebhookAnalysisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
